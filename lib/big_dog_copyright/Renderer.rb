59583a44d120ecac957392861e4e3ff8780368c9
Steve Hunter <steve.hunter@bigdogconsultants.co.uk>
2019-10-21 15:57:23 +0100

    amending Renderer class to try to make it work

--- lib/big_dog_copyright/Renderer.rb
+++ lib/big_dog_copyright/Renderer.rb
@@ -1,11 +1,9 @@
 module BigDogCopyright
   class Error < StandardError; end
   # Your code goes here...
-  module BigDogViewTool
-		class Renderer
-			def self.copyright name, msg
-				"&copy; #{Time.now.year} | <b>#{name}</b> | #{msg}".html_safe
-			end
+	class Renderer
+		def self.copyright name, msg
+			"&copy; #{Time.now.year} | <b>#{name}</b> | #{msg}".html_safe
 		end
 	end
 end
